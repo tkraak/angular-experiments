2b1d0ae6163026dd5e5cdde3fbddb2d9d6e7c35a
Tom Kraak <thomas.kraak@teamaol.com>
2016-10-25 13:49:35 -0400

    initial commit :baby:

---
+++ app.js
@@ -0,0 +1,25 @@
+const path = require('path')
+const htmlStandards = require('reshape-standard')
+const cssStandards = require('spike-css-standards')
+const jsStandards = require('babel-preset-latest')
+const pageId = require('spike-page-id')
+
+module.exports = {
+  devtool: 'source-map',
+  matchers: {
+    html: '*(**/)*.sgr',
+    css: '*(**/)*.sss'
+  },
+  ignore: ['**/layout.sgr', '**/_*', '**/.*', '_cache/**', 'readme.md'],
+  reshape: (ctx) => {
+    return htmlStandards({
+      webpack: ctx,
+      locals: { pageId: pageId(ctx), foo: 'bar' }
+    })
+  },
+  postcss: (ctx) => {
+    return cssStandards({ webpack: ctx })
+  },
+  babel: { presets: [jsStandards] },
+  server: { open: false }
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+## Angular dayparts
+
+Experimenting with the [Angular dayparts](https://github.com/cichys/angular-dayparts) directive.
+
